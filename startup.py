0672fe1b4240a6c7af86ea08a90e81c90513721b
troyraen <troy.raen@pitt.edu>
2019-02-11 11:34:06 -0500

    change get_stats -> calc_wtheta

--- startup.py
+++ startup.py
@@ -1,7 +1,9 @@
 import setup_halos as sh
-import get_stats as gs
+import calc_wtheta as cw
 import numpy as np
+%matplotlib qt
+import myplots as mp
 
 halocat, HODmodel = sh.setup_halos()
 bins = np.logspace(np.log10(5.0), np.log10(15.0), 15)
-bcens, wtheta = gs.get_stats(halocat, HODmodel, bins)
+bcens, wtheta = cw.calc_wtheta(halocat, HODmodel, bins)
