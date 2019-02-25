d009a8a3bfadde858b1abcbaebc7644e283727da
troyraen <troy.raen@pitt.edu>
2019-02-25 11:32:03 -0500

    move functions

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 # %matplotlib qt
 import numpy as np
+import pandas as pd
 from astropy import cosmology
 from astropy.table import Table
 
 import setup_mock as sm
 import calc_wtheta as cw
 import myplots as mp
+import helper_fncs as hf
+
 
 # Halotools assumes all lengths are in Mpc/h
 H0 = 70.0
@@ -14,56 +17,41 @@ try:
 except:
     halocat, HODmodel = sm.setup_halosHOD() # fetch halo catalog and HODmodel, returns populated HODmodel.mock
     # HODmodel.mock.populate() # repopulate
-    boxsize_og = halocat.Lbox[0]
-    zred = halocat.redshift
+    catLbox = halocat.Lbox[0]
+    catboxz = halocat.redshift
 
 galaxy_table = HODmodel.mock.galaxy_table # get the galaxy_table
 # mp.plot_galaxies(galaxy_table, gal_frac=0.005, coords='xyz') # plot a random subsample of galaxies
 
-# stack Nstack boxes together to create a bigger box
+# stack Nstack^3 boxes together to create a bigger box
 Nstack = 2
-boxsize = boxsize_og*Nstack
-newgals = sm.stack_boxes(galaxy_table, Nstack=Nstack, Lbox=boxsize_og)
+newLbox = catLbox*Nstack
+newgals = sm.stack_boxes(galaxy_table, Nstack=Nstack, Lbox=catLbox) # returns (ngals x 6) ndarray
 # ngtbl = Table(newgals, names=['x','y','z','vx','vy','vz'])
 # mp.plot_galaxies(ngtbl, gal_frac=5e-4, coords='xyz')
 
 # push the box out to x -> x + comoving_distance(halocat redshift)
 cosmo = cosmology.FlatLambdaCDM(H0=H0, Om0=0.3)
-# cosmo.comoving_distance(zred) returns "Comoving distance in Mpc to each input redshift.""
-# from help(cosmo): Dimensionless Hubble constant: h = H_0 / 100 [km/sec/Mpc]
-xzbox = (cosmo.comoving_distance(zred).value)*cosmo.h # Mpc/h
-newgals[:,0] = newgals[:,0]+ xzbox
-# ngtbl = Table(newgals, names=['x','y','z','vx','vy','vz'])
-# mp.plot_galaxies(ngtbl, gal_frac=5e-4, coords='xyz')
-
-# transform to ra, dec, and redshift
-rdz = sm.get_ra_dec_z(newgals, cosmo=cosmo, usevel=False)
-# mp.plot_galaxies(rdz, gal_frac=5e-4, coords='rdz')
-
-
-# # again with wrong cosmology
-# newgals[:,0] = newgals[:,0]- xzbox
-# cosmo = cosmology.FlatLambdaCDM(H0=0.7, Om0=0.3)
-# xzbox = (cosmo.comoving_distance(zred).value)*cosmo.h # Mpc/h
+newgals_atz = sm.push_box2z(newgals, catboxz, newLbox, cosmo=cosmo) # returns original ndarray with 1st column shifted
+# xzbox = (cosmo.comoving_distance(catboxz).value)*cosmo.h # Mpc/h
 # newgals[:,0] = newgals[:,0]+ xzbox
 # ngtbl = Table(newgals, names=['x','y','z','vx','vy','vz'])
 # mp.plot_galaxies(ngtbl, gal_frac=5e-4, coords='xyz')
 
+# transform to ra, dec, and redshift
+# rdzF = pd.DataFrame(hf.get_ra_dec_z(newgals, cosmo=cosmo, usevel=False), columns=['RA','DEC','Redshift'])
+# rdzT = pd.DataFrame(hf.get_ra_dec_z(newgals, cosmo=cosmo, usevel=True), columns=['RA','DEC','Redshift'])
+# mp.plot_galaxies(rdz, gal_frac=5e-4, coords='rdz')
+rdz = hf.get_ra_dec_z(newgals_atz, cosmo=cosmo, usevel=True) # now returns a df
+# rdz = rdzT
 
+# bin redshifts
+# hf.bin_redshifs(rdz, zspace)
+# for each mask, get slice of rdz and calc wtheta
+# save results in df (and write to file) with
+    # columns: {wtheta bcens (1 col each, holds wtheta for the bin),
+    #           zbin (hold zbcens value for given row),
+    #           mock number ()}
+    # rows: {zbcen (1 row each)}
 
-
-# #### SAND ####
-# gt = galaxy_table[1:5]
-# newgals = sm.stack_boxes(gt, Nstack=4, Lbox=boxsize_og)
-# mp.plot_galaxies(gt, gal_frac=1, coords='xyz')
-# ng = Table(newgals, names=['x','y','z','vx','vy','vz'])
-# #### SAND ####
-#
-# # get box as dataframe with ra, dec, z
-# # slice the box in redshift bins
-# # calculate and store wtheta for each zbin
-#
-# bins = np.logspace(np.log10(5.0), np.log10(15.0), 15)
-# bcens, wtheta = cw.get_wtheta(halocat, HODmodel, bins, fout='wtheta.dat')
-#
-# mp.plot_wtheta(bcens, wtheta)
+# then calculate wtheta
