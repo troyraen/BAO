919b279e489ca12ef2ecc832b09eb9fe0d06d3aa
troyraen <troy.raen@pitt.edu>
2019-02-27 14:13:30 -0500

    print zzz run times to file

--- main.py
+++ main.py
@@ -54,7 +54,9 @@ rdz = hf.get_ra_dec_z(newgals_atz, cosmo=cosmo, usevel=True) # now returns a df
 # rdz = rdzT
 
 # bin redshifts
-zspace = 0.365 # max redshift error in SDSS DR10 Photoz table is 0.365106
+zspace = 0.365 # max redshift error in SDSS DR10 Photoz table is 0.365106,
+                # see http://skyserver.sdss.org/CasJobs/MyDB.aspx MyTable_1 and
+                # http://skyserver.sdss.org/dr6/en/help/docs/algorithm.asp?key=photoz
 rdz, zbin_edges = hf.bin_redshifs(rdz, zspace=zspace, validate=False)
 print('\nYou should fix redshift bins so you get consistent binning with different mocks.\n')
 # get set of zbin centers to use as masks
@@ -63,6 +65,7 @@ zbcens = rdz.zbin.unique()
 tbins = np.logspace(np.log10(1.0), np.log10(10.0), 15)
 randoms_kwargs = { 'boxsize':newLbox, 'push_to_z':catboxz, 'cosmo':cosmo }
 fout = 'wtheta.dat'
+zrunfout = 'zruntime.dat'
 dtm = datetime.datetime.now() # get date and time to use as mock number
 mocknum = float(dtm.strftime("%m%d%y.%H%M"))
 nthreads = 24
@@ -72,13 +75,21 @@ for zzz in zbcens:
     rdz_z = rdz.loc[rdz.zbin == zzz]
     tbcens, wtheta = cw.calc_wtheta(rdz_z, tbins, randoms_kwargs, nthreads=nthreads)
     cw.write_to_file(tbcens, wtheta, zzz, mocknum, fout)
+
     end_zbin = time.time() # time the wtheta calculation
-    print('wtheta calculation took {0:.4f} seconds with nthreads = {1}\n'.format(end_zbin-start_zbin, nthreads))
+    ztime = (end_zbin-start_zbin)/60. # in minutes
+    print('wtheta calculation took {0:.1f} minutes with nthreads = {1}\n'.format(ztime, nthreads))
+    # zrunhdr = ['nthreads', 'zbin', 'calc time [min]', '# galaxies']
+    zrundat = np.asarray([nthreads, zzz, ztime, len(rdz_z.index)])
+    zrunstr = np.array2string(zrundat, formatter={'float_kind':lambda x: "%15.1f" % x})[1:-1])
+    print(zrunstr, file=open(zrunfout, 'a'))
+
 
 wdf = cw.load_from_file(fout)
 
 end_script = time.time() # time the script
-print('\n\t{0}\nmain.py ran for {1:.4f} seconds.\n'.format(datetime.datetime.now(), end_script-start_script))
+stime = (end_script-start_script)/60. # in minutes
+print('\n\t{0}\nmain.py ran for {0:.1f} minutes.\n'.format(datetime.datetime.now(), stime))
 
 
 
