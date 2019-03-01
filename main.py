cd90a1dfc9ce3f1aa6826be82356cfa52ed9422e
troyraen <troy.raen@pitt.edu>
2019-02-28 19:11:43 -0500

    fix import

--- main.py
+++ main.py
@@ -9,20 +9,17 @@ import datetime
 import time
 
 # import setup_mock as sm
-impoort do_mock_wtheta as dmw
+import do_mock_wtheta as dmw
 import calc_wtheta as cw
 import myplots as mp
 # import helper_fncs as hf
 
-# start_script = time.time() # time the script
-# print('\nmain.py started at {}'.format(datetime.datetime.now()))
 
 fout = 'wtheta.dat'
 zrunfout='zruntime.dat'
 nthreads = 32
 # tbins = np.logspace(np.log10(4.0), np.log10(8.0), 20)
-tbins = np.linspace(4.0, 8.0, 20)
-# if you change tbins, move the existing file ### FIX THIS ### (bash code)
+tbins = np.linspace(4.0, 8.0, 20) # if you change tbins, move the existing file ### FIX THIS ### (bash code)
 # !tm=$(date +"%m%d%y_%H%M")
 # !mv data/wtheta.dat data/wtheta_ow_${tm}.dat
 #
@@ -31,11 +28,6 @@ dmw.getmock_calcwtheta(Nstack=2, zspace=0.365, tbins=tbins, \
 
 fin = fout
 wdf = cw.load_from_file(fin)
-# pd.pivot_table(wdf, index='zbin')
+wdfp = pd.pivot_table(wdf, index='zbin')
 
-zrf = mp.getplot_zruntimes() # get and plot zrun calc times
-
-
-# end_script = time.time() # time the script
-# stime = (end_script-start_script)/60. # in minutes
-# print('\n\t{0}\nmain.py ran for {0:.1f} minutes.\n'.format(datetime.datetime.now(), stime))
+# zrf = mp.getplot_zruntimes() # get and plot zrun calc times
