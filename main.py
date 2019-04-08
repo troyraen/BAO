3fe7791bb1f5115cb94edea7ca51da8fe810f23e
troyraen <troy.raen@pitt.edu>
2019-04-08 12:24:05 -0400

    fix last

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from MockBox import MockBox as MB
 tbin_edges = np.logspace(np.log10(0.1), np.log10(15.0), 75)
 zbin_width = 0.4 # [0.3, 0.4, 0.5]
 # for zw in zbin_width:
+zw = zbin_width
 for i in range(10):
     mb = MB(Nstack=2, zbin_width=zw, tbin_edges=tbin_edges, galplots=False)
     mb.getmock_calcwtheta()
