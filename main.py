8a9e4e8a6fe70f87e2768168a494006f1e8c3315
troyraen <troy.raen@pitt.edu>
2019-04-08 12:22:28 -0400

    update main

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# %matplotlib
 import numpy as np
 import pandas as pd
-# import matplotlib.pyplot as plt
-# import matplotlib as mpl
-# from astropy import cosmology
-# from astropy.table import Table
-# import datetime
-# import time
-
 from MockBox import MockBox as MB
 # import setup as su
-# import do_mock_wtheta as dmw
 # import calc_wtheta as cw
 # import myplots as mp
 # import helper_fncs as hf
@@ -20,29 +11,9 @@ from MockBox import MockBox as MB
 # for operation speed comparisons
 #
 
-tbin_edges = np.logspace(np.log10(2.0), np.log10(15.0), 50)
-zbin_width = [0.3, 0.4, 0.5]
-for zw in zbin_width:
-    for i in range(10):
-        mb = MB()
-        mb.getmock_calcwtheta(Nstack=2, zbin_width=zw, tbin_edges=tbin_edges, galplots=False)
-
-
-
-### OLD ###
-
-# # gen mock, calc wtheta, and write to file
-# fout = 'data/wtheta.dat'
-# zrunfout='data/zruntime.dat'
-# nthreads = 32
-# tbins = np.logspace(np.log10(1.0), np.log10(13.0), 50)
-# # tbins = np.linspace(4.0, 8.0, 20)
-# su.load_popmock()
-# z4push = su.catboxz
-# galplots=False
-# zspace=0.365
-# Nstack=2
-# # for z4push in [0., su.catboxz]:
-# dmw.getmock_calcwtheta(Nstack=Nstack, z4push=z4push, zspace=zspace, tbins=tbins, \
-#         fout=fout, zrunfout=zrunfout, nthreads=nthreads, galplots=galplots)
-# ###
+tbin_edges = np.logspace(np.log10(0.1), np.log10(15.0), 75)
+zbin_width = 0.4 # [0.3, 0.4, 0.5]
+# for zw in zbin_width:
+for i in range(10):
+    mb = MB(Nstack=2, zbin_width=zw, tbin_edges=tbin_edges, galplots=False)
+    mb.getmock_calcwtheta()
