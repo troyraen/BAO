3c0cf5f0f537219e38a1c6c74140119c464e9692
troyraen <troy.raen@pitt.edu>
2019-04-10 09:12:24 -0400

    sample theta bins with linspace

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ from MockBox import MockBox as MB
 # for operation speed comparisons
 #
 
-tbin_edges = np.logspace(np.log10(0.1), np.log10(15.0), 75)
+tbin_edges = np.linspace(np.log10(0.1), np.log10(15.0), 75)
 zbin_width = 0.4 # [0.3, 0.4, 0.5]
 # for zw in zbin_width:
 zw = zbin_width
-for i in range(10):
+imax=30
+for i in range(imax):
+    print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
     mb = MB(Nstack=2, zbin_width=zw, tbin_edges=tbin_edges, galplots=False)
     mb.getmock_calcwtheta(fow='all')
