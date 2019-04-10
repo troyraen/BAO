729f2fa58af23c1244bd7e4de78bd12515693fff
troyraen <troy.raen@pitt.edu>
2019-04-10 09:43:33 -0400

    Do Nrands=10*cat_gals

--- main.py
+++ main.py
@@ -11,13 +11,15 @@ from MockBox import MockBox as MB
 # for operation speed comparisons
 #
 
+cat_gals = 5e5 # approx num gals in cat mock
+Nrands = 10*cat_gals # cat has ~5e5 galaxies
 # tbin_edges = np.logspace(np.log10(0.1), np.log10(15.0), 75)
 tbin_edges = np.linspace(0.1, 15.0, 75)
 zbin_width = 0.4 # [0.3, 0.4, 0.5]
 # for zw in zbin_width:
 zw = zbin_width
-imax=30
+imax=10
 for i in range(imax):
     print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
-    mb = MB(Nstack=2, zbin_width=zw, tbin_edges=tbin_edges, galplots=False)
-    mb.getmock_calcwtheta(fow='all')
+    mb = MB(Nstack=2, zbin_width=zw, tbin_edges=tbin_edges, Nrands=Nrands, galplots=False)
+    mb.getmock_calcwtheta()#fow='all')
