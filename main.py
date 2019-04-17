00348a644631018e0aa8c482940603d4fa1bcf83
troyraen <troy.raen@pitt.edu>
2019-04-17 10:01:28 -0400

    run z4push=0

--- main.py
+++ main.py
@@ -14,10 +14,11 @@ import myplots as mp
 
 tbin_edges = np.logspace(np.log10(0.1), np.log10(11.0), 76)
 # tbin_edges = np.linspace(0.1, 6.0, 51)
-zw = 0.4 # [0.3, 0.4, 0.5] # zbin width
+z4push = 0.0
+zw = 1.0 # 0.4 # [0.3, 0.4, 0.5] # zbin width
 cat_gals = 5e5 # approx num gals in cat mock
-Nstack_lst = [0, 2]
-nrfact_lst = [1,3,10]
+Nstack_lst = [0]#, 2]
+nrfact_lst = [1]#,3,10]
 
 for Nstack in Nstack_lst:
     print('\n-------------------------------------------------------------------')
@@ -31,7 +32,7 @@ for Nstack in Nstack_lst:
         imax=10
         for i in range(imax):
             print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
-            mb = MB(Nstack=Nstack, zbin_width=zw, tbin_edges=tbin_edges, Nrands=Nrands, galplots=False)
+            mb = MB(Nstack=Nstack, z4push=z4push, zbin_width=zw, tbin_edges=tbin_edges, Nrands=Nrands, galplots=False)
             mb.getmock_calcwtheta(nthreads=24)
 
 
