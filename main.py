d1394e30edf760cf72a3e00999c802644f292ddf
troyraen <troy.raen@pitt.edu>
2019-05-01 16:24:50 -0400

    test calc_stats()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
-from MockBox import MockBox as MB
+from MockBox import MockBox as MBox
 # import setup as su
 import calc_wtheta as cw
 import myplots as mp
@@ -11,15 +11,18 @@ import myplots as mp
 # for operation speed comparisons
 #
 
-
-tbin_edges = np.logspace(np.log10(0.1), np.log10(8.0), 76)
+stats=['wtheta']
+nbins = 53
+tbin_edges = np.logspace(np.log10(0.1), np.log10(8.0), nbins+1)
+rbin_edges = np.logspace(np.log10(100.0), np.log10(160.0), nbins+1)
 # tbin_edges = np.linspace(0.1, 6.0, 51)
 # tbin_edges = np.logspace(np.log10(0.5), np.log10(15.0), 76)
 z4push = 'cat'
-zw = 0.6 # 0.4 # [0.3, 0.4, 0.5] # zbin width
+zw = 0.2 # 0.4 # [0.3, 0.4, 0.5] # zbin width
 cat_gals = 5e5 # approx num gals in cat mock
 Nstack_lst = [0]#, 2]
 nrfact_lst = [1]#,3,10]
+imax = 1 # number of times to run each param combo
 
 for Nstack in Nstack_lst:
     print('\n-------------------------------------------------------------------')
@@ -30,11 +33,12 @@ for Nstack in Nstack_lst:
         print('\n*** Starting nrfact = {}\n\n'.format(nrfact))
 
         Nrands = int(nrfact*cat_gals* max(Nstack,1)**3)
-        imax=25
         for i in range(imax):
             print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
-            mb = MB(Nstack=Nstack, z4push=z4push, zbin_width=zw, tbin_edges=tbin_edges, Nrands=Nrands, galplots=False)
-            mb.getmock_calcwtheta(nthreads=24)
+            mb = MBox(Nstack=Nstack, z4push=z4push, zbin_width=zw, tbin_edges=tbin_edges, rbin_edges=rbin_edges, Nrands=Nrands, galplots=False)
+            # mb.getmock_calcwtheta(nthreads=24)
+            mb.getmock()
+            mb.calc_stats(stats=stats)
 
 
 # get and plot wtheta from file
