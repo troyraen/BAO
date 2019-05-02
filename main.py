f7b933143abe8f667f299fb22ec4899a1a24b172
troyraen <troy.raen@pitt.edu>
2019-05-02 14:12:12 -0400

    main.py test of calc_stats.py

--- main.py
+++ main.py
@@ -11,37 +11,52 @@ import myplots as mp
 # for operation speed comparisons
 #
 
-stats=['wtheta']
-nbins = 53
-tbin_edges = np.logspace(np.log10(0.1), np.log10(8.0), nbins+1)
+# DEFAULTS:
+stats=['wtheta', 'xi', 'wp']
+nbins = 50
+tbin_edges = np.logspace(np.log10(0.1), np.log10(12.0), nbins+1)
 rbin_edges = np.logspace(np.log10(100.0), np.log10(160.0), nbins+1)
-# tbin_edges = np.linspace(0.1, 6.0, 51)
-# tbin_edges = np.logspace(np.log10(0.5), np.log10(15.0), 76)
+pimax = 500
 z4push = 'cat'
-zw = 0.2 # 0.4 # [0.3, 0.4, 0.5] # zbin width
+zw = 2.
 cat_gals = 5e5 # approx num gals in cat mock
-Nstack_lst = [0]#, 2]
-nrfact_lst = [1]#,3,10]
+Nstack = 0 # number of mock boxes to stack, per dimension
+nrfact = 3 # used in Nrands
+Nrands = int(nrfact*cat_gals* max(Nstack,1)**3)
 imax = 1 # number of times to run each param combo
 
-for Nstack in Nstack_lst:
-    print('\n-------------------------------------------------------------------')
-    print('\n*** Starting Nstack = {}\n\n'.format(Nstack))
 
-    for nrfact in nrfact_lst:
-        print('\n____________________________________________________________________')
-        print('\n*** Starting nrfact = {}\n\n'.format(nrfact))
-
-        Nrands = int(nrfact*cat_gals* max(Nstack,1)**3)
-        for i in range(imax):
-            print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
-            mb = MBox(Nstack=Nstack, z4push=z4push, zbin_width=zw, tbin_edges=tbin_edges, rbin_edges=rbin_edges, Nrands=Nrands, galplots=False)
-            # mb.getmock_calcwtheta(nthreads=24)
-            mb.getmock()
-            mb.calc_stats(stats=stats)
+for i in range(imax):
+    print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
+    mb = MBox(Nstack=Nstack, z4push=z4push, zbin_width=zw, tbin_edges=tbin_edges, \
+                rbin_edges=rbin_edges, pimax=pimax, Nrands=Nrands, galplots=False)
+    mb.getmock()
+    mb.calc_stats(stats=stats, nthreads=24)
 
 
-# get and plot wtheta from file
+
+
+#
+# Nstack_lst = [0]#, 2]
+# nrfact_lst = [1]#,3,10]
+# for Nstack in Nstack_lst:
+#     print('\n-------------------------------------------------------------------')
+#     print('\n*** Starting Nstack = {}\n\n'.format(Nstack))
+#
+#     for nrfact in nrfact_lst:
+#         print('\n____________________________________________________________________')
+#         print('\n*** Starting nrfact = {}\n\n'.format(nrfact))
+#
+#         Nrands = int(nrfact*cat_gals* max(Nstack,1)**3)
+#         for i in range(imax):
+#             print('\n\n*** Starting interation {} out of {}\n\n'.format(i+1,imax))
+#             mb = MBox(Nstack=Nstack, z4push=z4push, zbin_width=zw, tbin_edges=tbin_edges, rbin_edges=rbin_edges, Nrands=Nrands, galplots=False)
+#             # mb.getmock_calcwtheta(nthreads=24)
+#             mb.getmock()
+#             mb.calc_stats(stats=stats)
+
+
+# # Get and plot wtheta from file
 # fin = 'data/wtheta.dat'
 # wdf = cw.load_from_file(fin)
 # wdf_nstackg = wdf.groupby('Nstack')
